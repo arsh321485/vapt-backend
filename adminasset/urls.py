0e06a63a55799c1d258ed71d1c122d0705ce6d24
bharti <Bharti@secureitlab.info>
2025-12-11 16:35:45 +0530

    admin asset search and delete api

--- adminasset/urls.py
+++ adminasset/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ReportAssetsAPIView
+from .views import ReportAssetsAPIView,AssetSearchAPIView,AssetDeleteAPIView
 
 urlpatterns = [
     path("report/<str:report_id>/assets/", ReportAssetsAPIView.as_view(), name="report-assets"),
+    path("assets/search/", AssetSearchAPIView.as_view(), name="report-asset-search"),
+    # delete host from report (host_name should be URL-encoded when calling)
+    path("report/<str:report_id>/assets/<path:host_name>/", AssetDeleteAPIView.as_view(), name="report-asset-delete"),
 ]
