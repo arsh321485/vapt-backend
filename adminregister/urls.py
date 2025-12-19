1217350b9e3c7e4906625b7e866e4dac9ebcdcd4
bharti <Bharti@secureitlab.info>
2025-12-19 12:57:16 +0530

    fix vulnerability api

--- adminregister/urls.py
+++ adminregister/urls.py
@@ -1,7 +1,12 @@
 # adminregister/urls.py
 from django.urls import path
-from .views import VulnerabilityRegisterAPIView
+from .views import VulnerabilityRegisterAPIView,FixVulnerabilityCreateAPIView
 
 urlpatterns = [
     path('register/<str:report_id>/vulns/', VulnerabilityRegisterAPIView.as_view(), name='report-vulns-by-id'),
+    path(
+        "fix-vulnerability/create/",
+        FixVulnerabilityCreateAPIView.as_view(),
+        name="fix-vulnerability-create"
+    ),
 ]
