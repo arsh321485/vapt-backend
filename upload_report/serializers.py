6296c3e96ddbfcc616ff62c255ac546aae687fa8
bharti <Bharti@secureitlab.info>
2026-01-11 00:23:48 +0530

    upload report get ,delete api

--- upload_report/serializers.py
+++ upload_report/serializers.py
@@ -3,22 +3,6 @@ from .models import UploadReport
 from users.models import User
 from location.models import Location
 
-# class UploadReportSerializer(serializers.ModelSerializer):
-#     location = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all(), required=False, allow_null=True)
-#     admin = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
-#     location_name = serializers.CharField(source='location.location_name', read_only=True)
-#     admin_email = serializers.CharField(source='admin.email', read_only=True)
-
-#     class Meta:
-#         model = UploadReport
-#         fields = [
-#             '_id', 'file', 'location', 'location_name',
-#             'admin', 'admin_email', 'member_type',
-#             'uploaded_at', 'status', 'parsed_count','created_at','updated_at'
-#         ]
-#         read_only_fields = ['_id', 'uploaded_at', 'created_at','updated_at','status', 'parsed_count']
-
-
 class UploadReportSerializer(serializers.ModelSerializer):
     _id = serializers.SerializerMethodField()
     location = serializers.SerializerMethodField()
@@ -54,21 +38,8 @@ class UploadReportSerializer(serializers.ModelSerializer):
     def get_admin(self, obj):
         return str(obj.admin.id) if obj.admin else None
     
-    # def get_file_url(self, obj):
-    #     request = self.context.get("request")
-    #     if obj.file and request:
-    #         return request.build_absolute_uri(obj.file.url)
-    #     return None   
-    
-    # def get_file(self, obj):
-    #     request = self.context.get("request")
-    #     if obj.file and request:
-    #         return request.build_absolute_uri(obj.file.url)
-    #     return None
-    
     def get_file(self, obj):
         request = self.context.get("request")
         if obj.file and request:
-            # obj.file.url -> /media/reports/xxx.html
             return request.build_absolute_uri(obj.file.url)
         return None
