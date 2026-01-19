08402dcfc0a21f0a218b21f3f1b15be4c0fbfc62
bharti <Bharti@secureitlab.info>
2026-01-19 17:42:40 +0530

    user details remove the location

--- users_details/admin.py
+++ users_details/admin.py
@@ -4,7 +4,7 @@ from .models import UserDetail
 
 @admin.register(UserDetail)
 class UserDetailAdmin(admin.ModelAdmin):
-    list_display = ("_id", "first_name", "last_name", "email", "user_type", "Member_role", "admin", "location", "created_at")
-    search_fields = ("first_name", "last_name", "email", "user_type", "Member_role", "admin__email", "location__location_name")
+    list_display = ("_id", "first_name", "last_name", "email", "user_type", "Member_role", "admin", "created_at")
+    search_fields = ("first_name", "last_name", "email", "user_type", "Member_role", "admin__email")
     list_filter = ("user_type", "Member_role", "created_at")
     readonly_fields = ("_id", "created_at", "updated_at")
--- users_details/serializers.py
+++ users_details/serializers.py
@@ -1,30 +1,65 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from bson import ObjectId
-from location.models import Location
+# from location.models import Location
 from .models import UserDetail
 
 User = get_user_model()
 
 class UserDetailSerializer(serializers.ModelSerializer):
     admin_id = serializers.CharField(source="admin.id", read_only=True)
-    location_id = serializers.CharField(source="location._id", read_only=True)
+    # location_id = serializers.CharField(source="location._id", read_only=True)
     Member_role = serializers.ListField(child=serializers.CharField(), read_only=True)
 
     class Meta:
         model = UserDetail
         fields = [
-            "_id", "admin_id", "location_id",
+            "_id", "admin_id", 
             "first_name", "last_name",
-            "user_type", "email", "select_location",
+            "user_type", "email", 
             "Member_role", "created_at", "updated_at"
         ]
         read_only_fields = ["_id", "created_at", "updated_at"]
 
 
+# class UserDetailCreateSerializer(serializers.ModelSerializer):
+#     admin_id = serializers.CharField(write_only=True)
+#     location_id = serializers.CharField(write_only=True)
+
+#     # allow list input
+#     Member_role = serializers.ListField(
+#         child=serializers.CharField(),
+#         allow_empty=False
+#     )
+
+#     class Meta:
+#         model = UserDetail
+#         fields = [
+#             "admin_id", "location_id",
+#             "first_name", "last_name",
+#             "user_type", "email", "select_location", "Member_role"
+#         ]
+
+#     def validate_admin_id(self, value):
+#         try:
+#             return User.objects.get(id=value)
+#         except User.DoesNotExist:
+#             raise serializers.ValidationError("Admin with this ID does not exist")
+
+#     def validate_location_id(self, value):
+#         try:
+#             return Location.objects.get(_id=ObjectId(value))
+#         except Location.DoesNotExist:
+#             raise serializers.ValidationError("Location with this ID does not exist")
+
+#     def create(self, validated_data):
+#         admin = validated_data.pop("admin_id")
+#         location = validated_data.pop("location_id")
+#         return UserDetail.objects.create(admin=admin, location=location, **validated_data)
+
+
 class UserDetailCreateSerializer(serializers.ModelSerializer):
     admin_id = serializers.CharField(write_only=True)
-    location_id = serializers.CharField(write_only=True)
 
     # allow list input
     Member_role = serializers.ListField(
@@ -35,9 +70,12 @@ class UserDetailCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetail
         fields = [
-            "admin_id", "location_id",
-            "first_name", "last_name",
-            "user_type", "email", "select_location", "Member_role"
+            "admin_id",
+            "first_name",
+            "last_name",
+            "user_type",
+            "email",
+            "Member_role"
         ]
 
     def validate_admin_id(self, value):
@@ -46,29 +84,70 @@ class UserDetailCreateSerializer(serializers.ModelSerializer):
         except User.DoesNotExist:
             raise serializers.ValidationError("Admin with this ID does not exist")
 
-    def validate_location_id(self, value):
-        try:
-            return Location.objects.get(_id=ObjectId(value))
-        except Location.DoesNotExist:
-            raise serializers.ValidationError("Location with this ID does not exist")
-
     def create(self, validated_data):
         admin = validated_data.pop("admin_id")
-        location = validated_data.pop("location_id")
-        return UserDetail.objects.create(admin=admin, location=location, **validated_data)
+        return UserDetail.objects.create(
+            admin=admin,
+            **validated_data
+        )
 
+# class UserDetailUpdateSerializer(serializers.ModelSerializer):
+#     admin_id = serializers.CharField(write_only=True, required=False)
+#     location_id = serializers.CharField(write_only=True, required=False)
+#     Member_role = serializers.ListField(child=serializers.CharField(), required=False)
 
+#     class Meta:
+#         model = UserDetail
+#         fields = [
+#             "admin_id", "location_id",
+#             "first_name", "last_name",
+#             "user_type", "email", "select_location", "Member_role"
+#         ]
+
+#     def validate_admin_id(self, value):
+#         if value:
+#             try:
+#                 return User.objects.get(id=value)
+#             except User.DoesNotExist:
+#                 raise serializers.ValidationError("Admin with this ID does not exist")
+#         return None
+
+#     def validate_location_id(self, value):
+#         if value:
+#             try:
+#                 return Location.objects.get(_id=ObjectId(value))
+#             except Location.DoesNotExist:
+#                 raise serializers.ValidationError("Location with this ID does not exist")
+#         return None
+
+#     def update(self, instance, validated_data):
+#         if "admin_id" in validated_data:
+#             instance.admin = validated_data.pop("admin_id")
+#         if "location_id" in validated_data:
+#             instance.location = validated_data.pop("location_id")
+
+#         for attr, value in validated_data.items():
+#             setattr(instance, attr, value)
+
+#         instance.save()
+#         return instance
+    
 class UserDetailUpdateSerializer(serializers.ModelSerializer):
     admin_id = serializers.CharField(write_only=True, required=False)
-    location_id = serializers.CharField(write_only=True, required=False)
-    Member_role = serializers.ListField(child=serializers.CharField(), required=False)
+    Member_role = serializers.ListField(
+        child=serializers.CharField(),
+        required=False
+    )
 
     class Meta:
         model = UserDetail
         fields = [
-            "admin_id", "location_id",
-            "first_name", "last_name",
-            "user_type", "email", "select_location", "Member_role"
+            "admin_id",
+            "first_name",
+            "last_name",
+            "user_type",
+            "email",
+            "Member_role"
         ]
 
     def validate_admin_id(self, value):
@@ -79,19 +158,9 @@ class UserDetailUpdateSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError("Admin with this ID does not exist")
         return None
 
-    def validate_location_id(self, value):
-        if value:
-            try:
-                return Location.objects.get(_id=ObjectId(value))
-            except Location.DoesNotExist:
-                raise serializers.ValidationError("Location with this ID does not exist")
-        return None
-
     def update(self, instance, validated_data):
         if "admin_id" in validated_data:
             instance.admin = validated_data.pop("admin_id")
-        if "location_id" in validated_data:
-            instance.location = validated_data.pop("location_id")
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
@@ -99,7 +168,7 @@ class UserDetailUpdateSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
     
-
+    
 class UserDetailRoleUpdateSerializer(serializers.Serializer):
     old_role = serializers.CharField(required=False, allow_blank=True)
     new_roles = serializers.ListField(child=serializers.CharField(), allow_empty=False)
