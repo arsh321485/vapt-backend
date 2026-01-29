7588b83ae52bd54cfd06581041ac7fddaa48222d
bharti <Bharti@secureitlab.info>
2026-01-29 14:29:09 +0530

    scope CRUD API

--- scope/admin.py
+++ scope/admin.py
@@ -1,27 +1,18 @@
 from django.contrib import admin
-from .models import Scope, ScopeFileUpload
-
-
-@admin.register(ScopeFileUpload)
-class ScopeFileUploadAdmin(admin.ModelAdmin):
-    list_display = ['_id', 'admin', 'file_name', 'file_type', 'targets_count', 'created_at']
-    list_filter = ['file_type', 'created_at']
-    search_fields = ['file_name', 'admin__email']
-    readonly_fields = ['_id', 'created_at']
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.select_related('admin')
+from .models import Scope, ScopeEntry
 
 
 @admin.register(Scope)
 class ScopeAdmin(admin.ModelAdmin):
-    list_display = ['_id', 'admin', 'target_type', 'target_value', 'testing_type', 'is_active', 'subnet_count', 'file_upload', 'created_at']
-    list_filter = ['target_type', 'is_active', 'created_at']
-    search_fields = ['target_value', 'admin__email', 'file_upload__file_name']
-    readonly_fields = ['_id', 'created_at', 'updated_at']
-    list_editable = ['is_active']
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.select_related('admin', 'file_upload')
+    list_display = ["name", "admin", "testing_type", "is_locked", "created_at"]
+    list_filter = ["testing_type", "is_locked", "created_at"]
+    search_fields = ["name", "admin__email"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+
+
+@admin.register(ScopeEntry)
+class ScopeEntryAdmin(admin.ModelAdmin):
+    list_display = ["value", "entry_type", "is_internal", "scope", "created_at"]
+    list_filter = ["entry_type", "is_internal", "created_at"]
+    search_fields = ["value", "scope__name"]
+    readonly_fields = ["id", "created_at", "updated_at"]
