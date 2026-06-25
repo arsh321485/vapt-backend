a9b0eaf7c319d99c87015951bdd1a8c5261e9dd9
bharti <Bharti@secureitlab.info>
2026-06-26 01:00:34 +0530

    common vulns

--- adminmitigationstrategy/tests.py
+++ adminmitigationstrategy/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from adminmitigationstrategy.views import (
+    MIN_MITIGATION_ASSET_COUNT,
+    _qualifies_for_mitigation_strategy,
+)
+
+
+class MitigationStrategyFilterTests(SimpleTestCase):
+    def test_min_asset_count_is_four(self):
+        self.assertEqual(MIN_MITIGATION_ASSET_COUNT, 4)
+
+    def test_qualifies_only_when_more_than_three_assets(self):
+        self.assertFalse(_qualifies_for_mitigation_strategy(1))
+        self.assertFalse(_qualifies_for_mitigation_strategy(2))
+        self.assertFalse(_qualifies_for_mitigation_strategy(3))
+        self.assertTrue(_qualifies_for_mitigation_strategy(4))
+        self.assertTrue(_qualifies_for_mitigation_strategy(5))
