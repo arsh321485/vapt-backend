cfc2e74c4cdc61527fc63580684f9b2be840c352
bharti <Bharti@secureitlab.info>
2026-06-30 15:26:10 +0530

    admin & user automated scripts

--- automation_scripts_api/urls.py
+++ automation_scripts_api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("match/<int:plugin_id>/", views.match_script, name="automation_script_match"),
-    path("match/bulk/", views.match_scripts_bulk, name="automation_script_bulk"),
-    path("", views.list_scripts, name="automation_scripts_list"),
+admin_urlpatterns = [
+    path("match/bulk/", views.admin_match_scripts_bulk, name="admin_automation_bulk"),
+    path("match/<int:plugin_id>/", views.admin_match_script, name="admin_automation_match"),
+    path("", views.admin_list_scripts, name="admin_automation_list"),
+]
+
+user_urlpatterns = [
+    path("match/bulk/", views.user_match_scripts_bulk, name="user_automation_bulk"),
+    path("match/<int:plugin_id>/", views.user_match_script, name="user_automation_match"),
+    path("", views.user_list_scripts, name="user_automation_list"),
 ]
